7bfc4be99d780d9be4b477bbb576cfb924a38e54
karranb <karranrb@gmail.com>
2022-10-08 07:54:07 -0300

    style: languages lint fixes

--- phaser_game/src/i18n/index.ts
+++ phaser_game/src/i18n/index.ts
@@ -3,16 +3,17 @@ import universalLanguageDetect from '@unly/universal-language-detector'
 import i18next from 'i18next'
 
 import en from './langs/en'
-import pt from './langs/pt'
+import fr from './langs/fr'
 import hu from './langs/hu'
 import ina from './langs/ina'
+import pt from './langs/pt'
 
 const lng = universalLanguageDetect({
-  supportedLanguages: ['en', 'pt', 'hu', 'ina'],
+  supportedLanguages: ['en', 'fr', 'pt', 'hu', 'ina'],
   fallbackLanguage: 'en',
 })
 
-const languages: Record<string, Record<string, string>> = { en, pt, hu, ina }
+const languages: Record<string, Record<string, string>> = { en, fr, pt, hu, ina }
 
 const resources = Object.entries(languages).reduce(
   (acc, [key, translation]) => ({
